dcf5740e3028253a2ca4f91c91c93f75f3c9f241
Horacio Bertorello <horacio.bertorello@gmail.com>
2023-12-01 12:44:45 -0300

    Done.

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,38 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name |> String.trim() |> String.at(0)
+  end
+
+  def initial(name) do
+    first_letter(name) |> String.upcase() |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    full_name
+    |> String.split(" ")
+    |> Enum.map(fn s -> initial(s) end)
+    |> Enum.join(" ")
+  end
+
+  def pair(full_name1, full_name2) do
+    fn1 = initials(full_name1)
+    fn2 = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{fn1}  +  #{fn2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
